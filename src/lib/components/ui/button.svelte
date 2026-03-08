a616858b8d7a63d32cd4c594d350c51ecbd18f56
sadikcivildef <sadikahasan6@gmail.com>
2026-03-08 21:54:43 +0600

    home page initial design

---
+++ src/lib/components/ui/button.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export let variant: "primary" | "outline" | "danger" = "primary";
+</script>
+
+<button class={`py-1 px-4 cursor-pointer ring-0 ring-primary/10 hover:ring-1 transition-all duration-100 rounded-md ${variant === "primary" ? "text-white  bg-primary hover:bg-primary/80" : variant === "outline" ? "border border-gray-400 hover:border-primary hover:text-primary" : "bg-default"}`}><slot/></button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import icon from '$lib/assets/favicon.svg';
+  import Button from '$lib/components/ui/button.svelte';
+</script>
+<div class="h-20 flex items-center justify-between px-10">
+    <div class="flex items-center text-2xl gap-3 text-amber-900">
+        <img src={icon} alt="" class="h-10"> Hana ERP
+    </div>
+
+    <ul class="flex items-center gap-8">
+        <li><a href="/about">About</a></li>
+        <li><a href="/learn">Learn</a></li>
+        <li><a href="/pricing">Pricing</a></li>
+        <li><Button>Login</Button></li>
+    </ul>
+</div>
+
+<div class="p-15 h-120 flex flex-col gap-10 items-center justify-center">
+    <div class=" text-6xl font-mono text-primary">Welcome to Hana ERP</div>
+    <div class="text-center max-w-270 leading-loose text-2xl text-gray-500">Hana ERP (SAP S/4HANA) leverages in-memory computing to deliver real-time data processing, streamlining complex business operations through an integrated, modern interface.</div>
+    <div class="flex items-center gap-5 text-xl">
+        <Button variant="outline">Learn</Button>
+        <Button>Login</Button>
+    </div>
+</div>
