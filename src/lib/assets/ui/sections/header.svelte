f7155977336f60d3058e33c79772b010742adf60
sadikcivildef <sadikahasan6@gmail.com>
2026-03-09 22:10:40 +0600

    mobile responsiveness and error page design

---
+++ src/lib/assets/ui/sections/header.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import icon from "$lib/assets/favicon.svg";
+  import Button from "$lib/assets/ui/components/button.svelte";
+  import Icon from "@iconify/svelte";
+  let menuOpen = false;
+</script>
+
+<div class="h-18 flex items-center justify-between px-10">
+  <div class="flex items-center text-2xl gap-3 text-amber-900">
+    <img src={icon} alt="" class="h-10" /> Hana ERP
+  </div>
+
+  <div class="relative flex items-center">
+    <ul class="flex items-center max-md:{menuOpen ? "": "hidden"} max-md:items-start max-md:gap-4  gap-8 max-md:absolute max-md:top-8 max-md:right-0 max-md:min-w-45 max-md:border rounded-md max-md:border-gray-300 max-md:p-3 bg-white max-md:flex-col max-md:shadow-md">
+      <li class="w-full"><a class="hover:text-primary block p-2 hover:bg-primary/20 rounded-md" href="/about">About</a></li>
+      <li class="w-full"><a class="hover:text-primary block p-2 hover:bg-primary/20 rounded-md" href="/learn">Learn</a></li>
+      <li class="w-full"><a class="hover:text-primary block p-2 hover:bg-primary/20 rounded-md" href="/pricing">Pricing</a></li>
+      <li class="w-full"><Button className="w-full">Login</Button></li>
+    </ul>
+    <button onclick={()=>menuOpen=!menuOpen} class="md:hidden cursor-pointer">
+      <Icon icon="humbleicons:bars" width="24" height="24" />
+    </button>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,22 @@
 <script lang="ts">
-    import icon from '$lib/assets/favicon.svg';
-  import Button from '$lib/components/ui/button.svelte';
-</script>
-<div class="h-20 flex items-center justify-between px-10">
-    <div class="flex items-center text-2xl gap-3 text-amber-900">
-        <img src={icon} alt="" class="h-10"> Hana ERP
-    </div>
+  import Button from "$lib/assets/ui/components/button.svelte";
+  import Header from "$lib/assets/ui/sections/header.svelte";
+  </script>
 
-    <ul class="flex items-center gap-8">
-        <li><a href="/about">About</a></li>
-        <li><a href="/learn">Learn</a></li>
-        <li><a href="/pricing">Pricing</a></li>
-        <li><Button>Login</Button></li>
-    </ul>
-</div>
+<Header/>
 
-<div class="p-15 h-120 flex flex-col gap-10 items-center justify-center">
-    <div class=" text-6xl font-mono text-primary">Welcome to Hana ERP</div>
-    <div class="text-center max-w-270 leading-loose text-2xl text-gray-500">Hana ERP (SAP S/4HANA) leverages in-memory computing to deliver real-time data processing, streamlining complex business operations through an integrated, modern interface.</div>
-    <div class="flex items-center gap-5 text-xl">
-        <Button variant="outline">Learn</Button>
-        <Button>Login</Button>
-    </div>
+
+<div
+  class="p-15 min-h-120 flex flex-col gap-10 items-center justify-center bg-[url(bg.svg)] bg-cover"
+>
+  <div class="text-center  text-6xl font-mono text-primary">Welcome <span class="max-md:hidden">to Hana ERP</span></div>
+  <div class="text-center max-w-270 leading-loose text-2xl text-gray-500">
+    Hana ERP (EXA 4HANA) leverages in-memory computing to deliver real-time data
+    processing, streamlining complex business operations through an integrated,
+    modern interface.
+  </div>
+  <div class="flex items-center gap-5 text-xl">
+    <Button variant="outline">Learn</Button>
+    <Button>Login</Button>
+  </div>
 </div>
