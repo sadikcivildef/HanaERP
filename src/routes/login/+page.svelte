316e123021ea08bfc05fa7597cf502421bff356f
sadikcivildef <sadikahasan6@gmail.com>
2026-03-10 19:07:35 +0600

    login page design fix

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -10,10 +10,12 @@
   class="min-h-[calc(100vh-4.5rem)] py-4 flex items-center justify-center bg-[url(bg.svg)] bg-cover"
 >
   <div
-    class="flex h-130 border border-primary/30 rounded-2xl shadow-lg overflow-hidden"
+    class="flex  w-full max-w-sm h-125 border border-primary/30 rounded-2xl shadow-lg overflow-hidden mx-5"
   >
-    <div class="flex flex-col items-center justify-center px-10 gap-6 bg-white">
-      <div class=" text-center">
+    <div
+      class="flex flex-col w-full items-center justify-center px-6 sm:px-8 gap-6 bg-white/60 backdrop-blur-lg"
+    >
+      <div class=" text-center mb-1">
         <div class="text-2xl text-primary font-semibold">
           Login to your account
         </div>
@@ -21,15 +23,15 @@
           Enter your email below to login to your account
         </div>
       </div>
-      <form action="" class="flex flex-col items-center gap-6">
-        <label class="flex flex-col gap-1 w-80">
+      <form action="" class="flex w-full flex-col items-center gap-6">
+        <label class="flex flex-col gap-1 w-full">
           <div class="text-sm">Email</div>
           <input
             class="px-4 py-1 rounded-md shadow-xs border border-gray-300 outline-none ring-0 ring-primary/20 focus:border-primary/70 focus:ring-3 transition-all ease-in-out"
             type="text"
           />
         </label>
-        <label class="flex flex-col gap-1 w-80">
+        <label class="flex flex-col gap-1 w-full">
           <div class="text-sm">Password</div>
           <input
             class="px-4 py-1 rounded-md shadow-xs border border-gray-300 outline-none ring-0 ring-primary/20 focus:border-primary/70 focus:ring-3 transition-all ease-in-out"
@@ -49,7 +51,12 @@
           className="w-full py-1.5 flex items-center justify-center gap-3"
           ><Icon icon="uim:google" width="18" height="18" /> Login with google</Button
         >
-        <div class="text-sm text-gray-500 text-center w-full">Don't have an account? <a href="/signup" class="hover:underline hover:text-primary">Sign up</a></div>
+        <div class="text-sm text-gray-500 text-center w-full">
+          Don't have an account? <a
+            href="/signup"
+            class="hover:underline hover:text-primary">Sign up</a
+          >
+        </div>
       </div>
     </div>
   </div>
