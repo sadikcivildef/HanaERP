cc6eae2083349341e84478e0161abc2a400e44f1
sadikcivildef <sadikahasan6@gmail.com>
2026-03-10 19:34:45 +0600

    minor design fix

--- src/lib/assets/ui/sections/header.svelte
+++ src/lib/assets/ui/sections/header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import icon from "$lib/assets/favicon.svg";
   import Button from "$lib/assets/ui/components/button.svelte";
   import Icon from "@iconify/svelte";
@@ -11,13 +12,14 @@
   </a>
 
   <div class="relative flex items-center">
-    <ul class="flex items-center max-md:{menuOpen ? "": "hidden"} max-md:items-start max-md:gap-4  gap-5 max-md:absolute max-md:top-8 max-md:right-0 max-md:min-w-45 max-md:border rounded-md max-md:border-gray-300 max-md:p-3 bg-white max-md:flex-col max-md:shadow-md">
-      <li class="w-full"><a class="hover:text-primary block px-3 py-1 hover:bg-primary/20 rounded-md" href="/about">About</a></li>
-      <li class="w-full"><a class="hover:text-primary block px-3 py-1 hover:bg-primary/20 rounded-md" href="/learn">Learn</a></li>
-      <li class="w-full"><a class="hover:text-primary block px-3 py-1 hover:bg-primary/20 rounded-md" href="/pricing">Pricing</a></li>
-      <li class="w-full"><Button className="w-full">Login</Button></li>
+    <ul class="flex z-50 items-center max-md:{menuOpen ? "": "hidden"} max-md:items-start  md:gap-5 max-md:absolute max-md:top-8 max-md:right-0 max-md:min-w-45 max-md:border rounded-md max-md:border-gray-300 md:p-3 bg-white max-md:flex-col max-md:shadow-md">
+      <li class="w-full"><a class="hover:text-primary block px-3 py-1 max-md:py-2 hover:bg-primary/20 md:rounded-md" href="/about">About</a></li>
+      <li class="w-full"><a class="hover:text-primary block px-3 py-1 max-md:py-2 hover:bg-primary/20 md:rounded-md" href="/learn">Learn</a></li>
+      <li class="w-full"><a class="hover:text-primary block px-3 py-1 max-md:py-2 hover:bg-primary/20 md:rounded-md" href="/pricing">Pricing</a></li>
+      <li class="w-full md:hidden px-3"><hr class="my-1 mb-3 border-gray-300 w-full"></li>
+      <li class="w-full max-md:px-3 max-md:pb-3"><Button className="w-full" onclick={()=>goto('/login')}>Login</Button></li>
     </ul>
-    <button onclick={()=>menuOpen=!menuOpen} class="md:hidden cursor-pointer">
+    <button onclick={()=>menuOpen=!menuOpen} class="md:hidden">
       <Icon icon="humbleicons:bars" width="24" height="24" />
     </button>
   </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import Button from "$lib/assets/ui/components/button.svelte";
   import Header from "$lib/assets/ui/sections/header.svelte";
   </script>
@@ -10,13 +11,13 @@
   class="p-15 min-h-120 flex flex-col gap-10 items-center justify-center bg-[url(bg.svg)] bg-cover"
 >
   <div class="text-center  text-6xl font-mono text-primary">Welcome <span class="max-md:hidden">to Hana ERP</span></div>
-  <div class="text-center max-w-270 leading-loose text-2xl text-gray-500">
+  <div class="text-center max-w-270 leading-7 sm:leading-loose text-2xl max-sm:text-xl text-gray-500">
     Hana ERP (EXA 4HANA) leverages in-memory computing to deliver real-time data
     processing, streamlining complex business operations through an integrated,
     modern interface.
   </div>
   <div class="flex items-center gap-5 text-xl">
     <Button variant="outline">Learn</Button>
-    <Button>Login</Button>
+    <Button onclick={()=>goto('/login')}>Login</Button>
   </div>
 </div>
