c840d1e3c611d42bdcb389c2960884c049f7228b
sadikcivildef <sadikahasan6@gmail.com>
2026-03-11 22:03:58 +0600

    initial dashboard design implement

--- src/routes/dashboard/ui/side-nav.svelte
+++ src/routes/dashboard/ui/side-nav.svelte
@@ -17,8 +17,76 @@
       <div class="px-4">Home</div>
       <ul class="text-sm text-black px-2 flex flex-col pt-2.5 gap-2">
         <li>
-          <a href="/dashboard" class="flex px-2 hover:bg-primary/5 rounded-md items-center gap-2 py-1.5"
-            ><Icon icon="tabler:dashboard" width="17" height="17" /> Dashboard</a
+          <a
+            href="/dashboard"
+            class="flex px-2 hover:bg-primary/5 rounded-md items-center gap-2 py-1.5"
+            ><Icon icon="tabler:dashboard" width="16" height="16" /> Dashboard</a
+          >
+        </li>
+        <li>
+          <a
+            href="/lifecycle"
+            class="flex px-2 hover:bg-primary/5 rounded-md items-center gap-2 py-1.5"
+            ><Icon
+              icon="carbon:ai-governance-lifecycle"
+              width="16"
+              height="16"
+            /> Lifecycle</a
+          >
+        </li>
+        <li>
+          <a
+            href="/lifecycle"
+            class="flex px-2 hover:bg-primary/5 rounded-md items-center gap-2 py-1.5"
+            ><Icon icon="boxicons:bar-chart-big" width="16" height="16" /> Analytics</a
+          >
+        </li>
+        <li>
+          <a
+            href="/lifecycle"
+            class="flex px-2 hover:bg-primary/5 rounded-md items-center gap-2 py-1.5"
+            ><Icon icon="ic:outline-folder" width="16" height="16" /> Projects</a
+          >
+        </li>
+        <li>
+          <a
+            href="/lifecycle"
+            class="flex px-2 hover:bg-primary/5 rounded-md items-center gap-2 py-1.5"
+            ><Icon icon="radix-icons:people" width="16" height="16" /> Projects</a
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="text-xs text-gray-500">
+      <div class="px-4">Documents</div>
+      <ul class="text-sm text-black px-2 flex flex-col pt-2.5 gap-2">
+        <li>
+          <a
+            href="/dashboard"
+            class="flex px-2 hover:bg-primary/5 rounded-md items-center gap-2 py-1.5"
+            ><Icon icon="bx:data" width="16" height="16" /> Data Library</a
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="text-xs text-gray-500">
+      <ul class="text-sm text-black px-2 flex flex-col pt-2.5 gap-2">
+        <li>
+          <a
+            href="/dashboard"
+            class="flex px-2 hover:bg-primary/5 rounded-md items-center gap-2 py-1.5"
+            ><Icon icon="clarity:clipboard-line" width="16" height="16" /> Reports</a
+          >
+        </li>
+      </ul>
+    </div>
+    <div class="text-xs text-gray-500">
+      <ul class="text-sm text-black px-2 flex flex-col pt-2.5 gap-2">
+        <li>
+          <a
+            href="/dashboard"
+            class="flex px-2 hover:bg-primary/5 rounded-md items-center gap-2 py-1.5"
+            ><Icon icon="tabler:file-word" width="16" height="16" /> Word Assistant</a
           >
         </li>
       </ul>
